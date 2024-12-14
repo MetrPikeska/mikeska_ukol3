8d2d7672e94dd300b52ad9597c79735900890a58
Petr Mikeska <161330167+MetrPikeska@users.noreply.github.com>
2024-12-15 00:22:50 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,8 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1XJ5GPk_YuZT9Y0R1WDAz4WfVnPlEozC_miqN7j8Zlfw/edit#gid=0';
-
-
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBmfyTS2y7tRhL1uY55-NtzA4h43VEY9DQ';
-
+var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
